6e6e77bf286538cfb29c32d1a0a522c8264cb313
administrator <administrator@administrators-Laptop.local>
2024-07-04 16:00:18 +0900

    hotfix:yml added

--- src/main/java/com/nhnacademy/eureka/Eureka.java
+++ src/main/java/com/nhnacademy/eureka/Eureka.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class Eureka {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Eureka.class, args);
     }
 
 }
